28d45be3caa32ce729408069d79abbf20a11cace
miang <2594416505@qq.com>
2024-03-01 14:48:53 +0800

    perf: json format --> str/int

--- config/parameters.py
+++ config/parameters.py
@@ -1,10 +1,10 @@
 # input your gpt base url:
-api_base: "xxx.com"
+api_base = "xxx.com"
 # input your gpt module:
-gpt_module: "gpt-4-0125-preview"
+gpt_module = "gpt-4-0125-preview"
 # input your gpt api:
-api_key: "xxxxxxxxxxxxxxx"
+api_key = "xxxxxxxxxxxxxxx"
 # input the timeout for process timeout(default 20s):
-process_time_out: 20
+process_time_out = 20
 
 
