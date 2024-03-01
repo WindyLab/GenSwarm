70c9f9f0bbf424638e35e4a52f894c49a578ae9c
miang <2594416505@qq.com>
2024-03-01 14:24:02 +0800

    feat: change parameters.yml --> parameters.py

---
+++ config/parameters.py
@@ -0,0 +1,9 @@
+# input your gpt base url:
+api_base: "xxx.com"
+# input your gpt module:
+gpt_module: "gpt-4-0125-preview"
+# input your gpt api:
+api_key: "xxxxxxxxxxxxxxx"
+# input the timeout for process timeout(default 20s):
+process_time_out: 20
+
